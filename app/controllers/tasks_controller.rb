30b6c439e7430ecd17d2e442945eb41b0cc012cf
Nissrine Canina <niss.canina@gmail.com>
2022-01-28 11:13:43 -0500

    Create post 'tasks' request with 'type'

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,18 @@ class TasksController < ApplicationController
         tasks =Task.all 
         tasks.to_json(include: [:type])
       end
+
+    post '/tasks' do 
+      type = Type.find_or_create_by(name: params[:type][:name])
+      task = type.tasks.build(params[:task])
+
+     if task.save
+      task.to_json(include: [:type])
+     else
+      { errors: task.errors.full_messages }.to_json
+     end
+
+
+    end
+
 end
