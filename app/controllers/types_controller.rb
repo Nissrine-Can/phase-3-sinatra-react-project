d213884f2699e33c120da22753e727118280bfb9
Nissrine Canina <niss.canina@gmail.com>
2022-01-27 19:22:39 -0500

    Add error message to show type route

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -8,7 +8,11 @@ class TypesController < ApplicationController
     # Types show route (display a specific type)
      get '/types/:id' do
          type = Type.find_by(id: params[:id])
+         if type 
          type.to_json(include: :tasks)
+         else
+           "404 - Type not found"
+         end
        end
 
 end
