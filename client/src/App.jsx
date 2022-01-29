020473a76a9e0b9bc13e03d0f21517e3f0b2eeb1
Nissrine Canina <niss.canina@gmail.com>
2022-01-29 10:58:12 -0500

    Build client Home, Tasks, Types components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
-import Navbar from "./components/Navbar";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Navbar from "./components/Navbar"
+import Home from "./components/Home"
+import Types from "./components/Types"
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
+    <div className="App">
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/" element={<Types />} />
+      </Routes>
     </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+        <div>
+           <h1>Task Management App</h1>
+           <hr/>
+           <p>Click the Types button to see a list of categories</p>
+        </div>
+        )
+}
+
+export default Home;
