d28ac0804b652c7b1be8761835160350245d6b21
Nissrine Canina <niss.canina@gmail.com>
2022-01-28 21:53:32 -0500

    Create Navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+  return(
+  <div>
+      <nav>
+          <ul>
+              <li>Home Page</li>
+              <li>List Tasks</li>
+              <li>Create Tasks</li>
+          </ul>
+      </nav>
+  </div>
+  )
+}
+
+export default Navbar
