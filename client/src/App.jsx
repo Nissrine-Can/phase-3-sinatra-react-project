8ddf7f1684a5e6562d00f6ea16b9b57bce2d69f5
Nissrine Canina <niss.canina@gmail.com>
2022-02-04 08:59:56 -0500

    Create Book state in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,26 +3,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './App.css'
 
-import React, { useState, useEffect } from 'react'
+import React , { useState, useEffect } from 'react';
+import Navbar from './components/navigation/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './containers/Home';
+import Home from './components/static/Home';
+import BookList from './components/books/BookList';
+import BookForm from './components/books/BookForm';
+import { baseUrl } from './Globals';
+
 
 
 function App() {
-   
+
+   const [books, setBooks] = useState([])
+
+   useEffect(() => {
+     fetch(baseUrl + "/books")
+     .then(res => res.json())
+     .then(data => setBooks(data))
+   }, [])
+
   return (
     <Router>
-       <Routes>
-         < Route path="/" element={ <Home /> } />
-         < Route path="/tasks"  />
-         < Route path="/tasks/new"  />
-         < Route path="/types/:id" />
-
-       </Routes>
-
-    </Router> 
-    
-  );
+         <Navbar />
+         <Routes>
+           <Route path="/" element={ <Home />} />
+           <Route path="/books" element={ <BookList books={ books }/>} />
+           <Route path="/books/new" element={ <BookForm/>} />
+         </Routes>
+    </Router>
+  )
 }
 
 export default App;
