90d2243f2130e007635e6932424e19ae9a530efd
Nissrine Canina <niss.canina@gmail.com>
2022-02-04 08:58:21 -0500

    Add Navbar links

---
+++ client/src/components/navigation/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/books">Book List</Link></li>
+                <li><Link to="/books/new">Add Book</Link></li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar;
