60f1939bb5a445f3f52975af62cd4e873440294b
Nissrine Canina <niss.canina@gmail.com>
2022-02-04 20:29:05 -0500

    Style Navbar

--- client/src/components/navigation/Navbar.jsx
+++ client/src/components/navigation/Navbar.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import Stack from 'react-bootstrap/Stack'
+
 
 const Navbar = () => {
   return (
-    <div>
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/books">Book List</Link></li>
-                <li><Link to="/books/new">Add Book</Link></li>
-            </ul>
-        </nav>
-    </div>
+      <>
+        <div className="header">
+           <h1 className="logo">Read Aloud Tracker</h1>
+           <h6>Grades K- 3</h6>
+        </div>
+        < div className="nav-header">
+           <Stack direction="horizontal" gap={3}>
+              <Link className="btn btn-primary" to="/">Home</Link>{' '}
+              <Link className="btn btn-primary" to="/books">Book List</Link>{' '}
+              <Link className="btn btn-primary" to="/books/new">Add Book</Link>
+            </Stack>
+         </div>
+    </>
   )
 }
+
 
 export default Navbar;
