987e2b03327394b4a02ad660855c4673cc756aa1
Nissrine Canina <niss.canina@gmail.com>
2022-02-05 13:40:26 -0500

    Add Post and nav to form

--- client/src/components/books/BookForm.jsx
+++ client/src/components/books/BookForm.jsx
@@ -1,49 +1,97 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { baseUrl, headers } from '../../Globals';
+import { useNavigate } from 'react-router-dom';
+const BookForm = ({ addBook, addErrors, clearErrors }) => {
+  
+  const [title, setTitle] = useState("")
+  const [author, setAuthor] = useState("")
+  const [summary, setSummary] = useState("")
+  
+  const navigate = useNavigate()
+  
+  useEffect(() => {
+    return () => {
+      clearErrors()
+    }
+  }, [])
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    const paramsData = {
+      author: {
+        name: author
+      },
+      book: {
+        title: title,
+        summary: summary
+      }
+    }
+   fetch(baseUrl + '/books',{
+     method: "POST",
+     headers,
+     body: JSON.stringify(paramsData)
+   })
+   .then(res => res.json())
+   .then(data => {
+     //console.log(data)
+     if (data.errors) {
+        addErrors(data.errors)
+     } else {
+      addBook(data)
+      navigate("/books")
+     }
+   })
+  }
 
-const BookForm = () => {
   return (
     <div className="form">
-
-           <div class="mb-3">
-                <label for="title" className="form-label" >Title</label>
+           <form onSubmit={ handleSubmit }>
+           <div className="mb-3">
+                <label htmlFor="title" className="form-label" >Title</label>
                 <input 
-                class="form-control"
+                className="form-control"
                 type="text" 
                 name="title" 
                 id="title"
-                placeholder="Enter Book Title"
+                placeholder="Enter book's title"
+                value={title}
+                onChange={ e => setTitle(e.target.value)}
                 />
           </div>
             
-          <div class="mb-3">
-              <label for="author" className="form-label">Author</label>
+          <div className="mb-3">
+              <label htmlFor="author" className="form-label">Author</label>
                 <input 
-                class="form-control"
+                className="form-control"
                 type="text" 
                 name="author" 
                 id="author"
                 placeholder="Enter author's name"
+                value={author}
+                onChange={ e => setAuthor(e.target.value)}
                 />
             </div>
           
-            <div class="mb-3">
-                    <label for="summary" class="form-label" >Summary</label>
+            <div className="mb-3">
+                    <label htmlFor="summary" className="form-label" >Summary</label>
                     <textarea
                     className="form-control"
-                    rows="3"
+                    rows="5"
                     type="text" 
                     name="summary" 
                     id="summary"
                     placeholder="Enter Summary"
+                    value={summary}
+                    onChange={ e => setSummary(e.target.value)}
                     />
             </div>
         
             <button 
-            type="button" 
-            class="btn btn-primary"
+            type="submit" 
+            className="btn btn-primary"
             >Add Book</button>
-            
+
+            </form>
       
     </div>
   )
