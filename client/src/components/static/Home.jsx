2c6d3b9ec86348ec99248c2519c4346a98598432
Nissrine Canina <niss.canina@gmail.com>
2022-02-06 18:38:55 -0500

    Add static elements

--- client/src/components/static/Home.jsx
+++ client/src/components/static/Home.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-       <h1>Home Page</h1>
+       <h1 className="home-title">Books for Grades K-3</h1>
+       <p className="home-intro">Children in grades K-3 are beginning to read on their own, but they still like to hear a story read to them. Here's a list of books our teacher editors suggest to meet both needs. Parents can use many of these as "read-alongs" to help their children improve their reading. Children can also read many of these books on their own. The reading levels vary, but all are at an appropriate "interest" level for primary grades. Have your child use the five finger test to decide if the book is too difficult and should be a "read together" choice.</p>
     </div>
   )
 }
