beb782fdb79afa6425994065475515a30fc416bc
Nissrine Canina <niss.canina@gmail.com>
2022-02-06 18:13:59 -0500

    Add authors route

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,13 +1,16 @@
 class AuthorsController < ApplicationController
     
+    get '/authors' do 
+        authors = Author.all 
+        authors.to_json(include: [:books])
+    end
+
     get '/authors/:id' do 
         find_author
         @author.to_json(include: [:books])
     end
 
-    
-
-    private
+ private
        def find_author
         @author = Author.find_by_id(params[:id])
        end
